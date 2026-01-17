960f1623b6b5e1aac5ef75d809ada4997376b931
xwwuhq <zlx336@gmail.com>
2026-01-18 00:01:23 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 if _G.D3X_LOADED then return end
 _G.D3X_LOADED = true
 
-local url = "https://raw.githubusercontent.com/TONUSER/D3X-Hub/main/core.lua"
+local url = "https://raw.githubusercontent.com/xwwuhq/d3x-premium/main/core.lua"
 loadstring(game:HttpGet(url))()
